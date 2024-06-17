38304906937a3af188821a13ee60cc656aef2f3c
brtiago <tgribeiro@gmail.com>
2024-06-17 16:09:29 -0300

    criação da classe UserRepository

--- src/main/java/edu/tiago/spring_security_jwt/service/UserService.java
+++ src/main/java/edu/tiago/spring_security_jwt/service/UserService.java
@@ -1,6 +1,7 @@
 package edu.tiago.spring_security_jwt.service;
 
 import edu.tiago.spring_security_jwt.model.User;
+import edu.tiago.spring_security_jwt.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
